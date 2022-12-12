c8b84dd21b641a4f942fda1248553011fd6bc027
Karamo <karamo.skhan@gmail.com>
2022-12-12 11:53:53 +0100

    modelSchema

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 const routes = require('./routes/userRoutes.js')
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(
+    `mongodb+srv://Karamo:123kar123@cluster0.jruaae3.mongodb.net/?retryWrites=true&w=majority`,
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    ()=> {
+        console.log('DB connected');
+    }
+);
+
+
+
 app.use(express.json());
 
 app.use('/api', routes);
